429613e4f84697e78b815f49d80670f3dc2d7f54
Gajani-uruthi <158301975+Gajani-uruthi@users.noreply.github.com>
2026-07-04 22:32:42 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';  //this will use if type is module in package.jso
 // const express = require('express');  this will use if type is commonjs in package.json
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
@@ -13,14 +14,19 @@ console.log(process.env.MONGO_URI); // to check if the environment variable is l
 
 const app = express();
 const PORT = process.env.PORT || 5001; // Use the PORT from environment variables or default to 5001
-
+const __dirname = path.resolve(); // Get the current directory name
 
 // Middleware to parse JSON bodies
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-app.use(
+
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(
     cors({
         origin: "http://localhost:5173",
     }));
+}
+
 
 // our simple custom middleware
 // app.use((req,res,next)=>{
@@ -33,6 +39,14 @@ app.use(rateLimiter); // Apply the rate limiter middleware to all routes
 // An endpoint is a combination of a URL + HTTP method (GET, POST, PUT, DELETE, etc.) that  lets the client  interact with a specific resource.
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))); // Serve static files from the frontend build directory
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html")); // Serve the frontend's index.html for any other routes
+});
+}
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
